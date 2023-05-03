b46b028716196b2357a624b20d2bd8e217d64225
sheiksaga <gandhi.sangeeth@gmail.com>
2023-05-03 23:04:03 +0200

    simplified 	modified:   assets/css/main.css 	modified:   assets/js/main.js 	new file:   assets/js/styles.js 	modified:   index.html

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
         return false;
     });
 });
-  
+
 //dark mode
 $(document).ready(function() {
     // check if user prefers dark mode
@@ -28,4 +28,27 @@ $(document).ready(function() {
         $('body').toggleClass('dark-mode');
         localStorage.setItem('dark-mode', $('body').hasClass('dark-mode').toString());
     });
+});
+
+
+  
+//fetch likes
+$(document).ready(function() {
+    var facts = [
+      "whether aliens like pizza", "the beauty of a perfectly brewed cup of coffee", "the magic of a well-written book", "why pineapples on pizza became socially accepted", "websites", "the latest LLM", "jabberwockys", "invisbile electric sheep", "how to construct the perfect pun"
+    ];
+  
+    function newFact() {
+      var randomFact = Math.floor(Math.random() * facts.length);
+      $('#likeDisplay').text(facts[randomFact]);
+    }
+  
+    newFact();
+  });
+
+  $.get("/blog/posts/2023/index.html", function(data) {
+    // Get the first <li> element and find the <a> tag inside it
+    var firstLink = $(data).find('li:first').find('a');
+    // Set the HTML of #2023 to the last link
+    $("#2023").html(firstLink);
 });
